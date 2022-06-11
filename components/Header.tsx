51a145adfb7c5c4996a6378e855343b178aca356
Johnny Luong <jluong.dev@gmail.com>
2022-06-11 01:29:34 -0700

    changed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
                 <div className='md:float-left block'>
                     <Link href='/'>
                         <span className='cursor-pointer font-bold text-4xl text-white'>
-                            johnny's blog
+                            johnny's tech blog
                         </span>
                     </Link>
                 </div>
